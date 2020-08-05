035dc74f3a9b1b6dc10d93ed4389009172a8a793
=baichonghua <18211167516@163.com>
2020-08-05 15:22:08 +0800

    day1

--- day1-base/example2/ebb/ebb.go
+++ day1-base/example2/ebb/ebb.go
@@ -5,13 +5,16 @@ import (
 	"log"
 )
 
+//定义框架请求处理方法
 type HandlerFunc func(w http.ResponseWriter,req *http.Request)
 
 
+//核心结构体
 type Engine struct{
-	router map[string]HandlerFunc
+	router map[string]HandlerFunc //简单使用map记录路由信息
 }
 
+//实例化结构体
 func New() *Engine{
 	engine := &Engine{
 		router : make(map[string]HandlerFunc),
@@ -19,6 +22,7 @@ func New() *Engine{
 	return engine
 }
 
+//添加到结构体路由
 func (engine *Engine) addRoute(mothod string,pattern string,handler HandlerFunc){
 	key := mothod+"-"+pattern
 	engine.router[key] = handler
@@ -32,11 +36,15 @@ func (engine *Engine) POST(pattern string,handler HandlerFunc){
 	engine.addRoute("POST",pattern,handler)
 }
 
+//启动服务
 func (engine *Engine) Run(addr string) (err error){
 	return http.ListenAndServe(addr,engine)
 }
 
+//engine 实现ServeHTTP接口（所有的请求都会走到这）
+//查找是否路由映射表存在，如果存在则调用，否则返回404
 func (engine *Engine) ServeHTTP(w http.ResponseWriter,req *http.Request){
+	
 	key := req.Method + "-" + req.URL.Path
 	if handler, ok := engine.router[key]; ok {
 		handler(w, req)
--- day1-base/example2/main.go
+++ day1-base/example2/main.go
@@ -9,14 +9,12 @@ import (
 func main(){
 	r := ebb.New()
 
-	r.GET("/",func(w http.ResponseWriter,req *http.Request){
+	r.GET("/index",func(w http.ResponseWriter,req *http.Request){
 		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
 	})
 
 	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+		fmt.Fprintln(w, req.Header)
 	})
 	
 	r.Run(":8080")
